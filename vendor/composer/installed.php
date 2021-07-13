93e48cb6eca59ad6edeeb6804b30d36ded074a3e
Josue-dev <52864843+Josue-dev@users.noreply.github.com>
2021-07-12 20:14:04 -0500

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '88369ff67fc99113456b11875a0856c42427b1bb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '88369ff67fc99113456b11875a0856c42427b1bb',
+    ),
+  ),
+);
